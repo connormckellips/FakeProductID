d7edd02de4e597e92d7949209f9cd037438c7948
RaffelNicholas <raffeln6264@my.uwstout.edu>
2022-04-10 17:13:11 -0500

    Started Code for the blockchain

--- FakePro.py
+++ FakePro.py
@@ -1,3 +1,14 @@
-print("FakePro For Block Chain and MSC 396")
-print("New Test")
-print("Test 2")
+import hashlib
+
+class BC:
+    def __init__(self, previousHash, transactionList):
+        self.previousHash = previousHash
+        self.transactionList = transactionList
+
+        #Creates the data to placed into each block
+        self.block_data = "-".join(transactionList) + "-" + previousHash
+        #Hashes with Sha256 hashing algorinthm
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+
+bc = BC("bob", "1")
+bc.displayBlock()
