51377a95bcf84371dfbc8df18cd9e301fc39dfe7
RaffelNicholas <raffeln6264@my.uwstout.edu>
2022-04-11 14:13:00 -0500

    Block Strings are now previous transaction Hashes to match blocks

--- FakePro.py
+++ FakePro.py
@@ -17,11 +17,17 @@ class BC:
 
 t1 = "Bob sends 5 bitcoin to John"
 t2 = "Wonton sends 1 bitcoin to John"
-t3 = "Wonton sends anita 3 Bitcoin"
-t4 = "Anita sends bob 2 bitcoin"
+t3 = "Wonton sends 3 bitcoin to Anita"
+t4 = "Anita sends 2 Bitcoin to Bob"
+t5 = "Wonton send 190 bitcoin to Hacker"
+t6 = "Hacker sends 200 bitcoin to Bossman"
+
 
 bc = BC("Initial string", [t1,t2])
 bc.displayBlock()
 print("--------------------")
 bc2 = BC(bc.block_hash, [t3,t4])
 bc2.displayBlock()
+print("--------------------")
+bc3 = BC(bc2.block_hash, [t5,t6])
+bc3.displayBlock()
