d529410e946f2701e4f7a580f7d3372b9407a34e
Connor McKellips <connor@mckellips.com>
2022-05-02 14:00:13 -0500

    Fixed a bug comparing monkey nft to book, and clarified some comments

--- FakePro.py
+++ FakePro.py
@@ -2,20 +2,20 @@
 #   Connor McKellips
 #   Nick Raffel
 #
-#   Applied Cryptography - MSCS 380
-#   This is a Coding assignment that demonstrates in a basic sense how block chain works
-#   it contains the entire block data of the item, company, store, owner, etc
-#   and creates a key hash based on that info
+#   Applied Cryptography - MSCS 396
+#   This is a Coding assignment that demonstrates basic blockchain cryptography
+#   A product block contains data of the item, company, store, owner, etc.
+#   It also creates a keyed hash based on that information
 #
 ############
 
 import hashlib
 
-# Class to set up the Block Chain, hashes, and ability to compare them
+# Class to set up the blockchain, hashes, and ability to compare them
 class BC:
     # initializes a product block upon creation
     def __init__(self, previousHash, company, item, owner):
-        # previous hash is a reference to the previous block
+        # creates the block's previous hash, company, item, and owner
         self.previousHash = previousHash
         self.company = company
         self.item = item
@@ -23,7 +23,7 @@ class BC:
 
         # Creates the data to placed into each block
         self.block_data = "".join(company) + " : " + "".join(item) + " : " + "".join(owner) + " | " + previousHash
-        # Hashes with Sha256 hashing algorinthm
+        # Hashes with Sha256 hashing algorithm
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
     # creates a predicted hash for product block
@@ -36,7 +36,7 @@ class BC:
 
     # compares the hashes with each other
     def compare(self, item):
-        # pre_hash item we are creating
+        # prints true if the block holds the correct hash value (product is validated)
         if self.pre_Hash == item.block_hash:
             print( "Hashes look right, this is a real token" )
         else:
@@ -44,12 +44,13 @@ class BC:
 
     # adds the product onto the list of products
     def displayProduct(self):
-        #self.predictHash()
+        # prints new hash information
         print("[", self.block_data, "|", "New Block Hash :", self.block_hash, "]")
 
 
 # simple function for input validation
 def check_option(answer):
+    # if the input is not a valid character, it prints false
     if not option.isdigit() or int(option) <= 0 or int(option) > 3:
         print("not an option")
         return False
@@ -57,7 +58,7 @@ def check_option(answer):
         return True
 
 
-# 61 - 63 Creates the Initial token
+# Creates the Initial token
 VideoGame = BC("Initialized", "CryptoMart", "VideoGame", "CryptoMart")
 Book = BC("Initialized", "CryptoMart", "Book", "CryptoMart")
 Monkey_NFT = BC("Initialized", "CryptoMart", "Monkey NFT", "CryptoMart")
@@ -77,10 +78,10 @@ while not check_option(option):
 
 # if check option is accepted
 if check_option(option):
-    # option 1 check
+    # option 1 (video game) check
     if option == "1":
         print("Oh, a ", VideoGame.item, "!\nHere's the Block with the Hashes!")
-        # creates the new book that you bought and dispays the packet
+        # creates the new book that you bought and displays the packet
         new_videogame = BC(VideoGame.block_hash, "CryptoMart", "VideoGame", name)
         new_videogame.displayProduct()
 
@@ -100,7 +101,7 @@ if check_option(option):
         else:
             print("Error, your hash was not the right hash.")
 
-    # option 2 check
+    # option 2 (book) check
     if option == "2":
         print("Oh, a ", Book.item, "!\nHere's the Block with the Hashes!")
         # creates the new book that you bought and dispays the packet
@@ -123,7 +124,7 @@ if check_option(option):
         else:
             print("Error your hash, was not the right hash.")
 
-    # option 3 check
+    # option 3 (monkey NFT) check
     if option == "3":
         print("Oh, a ", Monkey_NFT.item, "!\nHere's the Block with the Hashes!")
         # creates the new book that you bought and dispays the packet
@@ -131,7 +132,7 @@ if check_option(option):
         new_monkey_nft.displayProduct()
 
         # fake NFT that the "hacker" tried to steal while you bought it
-        fake_nft = BC(Book.block_hash, "CryptoMart", "Monkey NFT", "Hacker")
+        fake_nft = BC(Monkey_NFT.block_hash, "CryptoMart", "Monkey NFT", "Hacker")
         print("Looks like a hacker is trying to take your new Book!",
               "\nFortunately, we can compare the hashes to check the integrity")
         print("Your Hash:\t\t", new_monkey_nft.block_hash, "\nHacker's Hash:\t", fake_nft.block_hash)
@@ -145,4 +146,3 @@ if check_option(option):
             print("Your hash, is the accepted Hash!")
         else:
             print("Error your hash, was not the right hash.")
-
