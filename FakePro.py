cb20fa7c7dd192a7fd58c94b04ce0a938519bf22
RaffelNicholas <raffeln6264@my.uwstout.edu>
2022-05-02 13:32:01 -0500

    Documented code.

--- FakePro.py
+++ FakePro.py
@@ -11,7 +11,7 @@
 
 import hashlib
 
-
+# Class to set up the Block Chain, hashes, and ability to compare them
 class BC:
     # initializes a product block upon creation
     def __init__(self, previousHash, company, item, owner):
@@ -48,6 +48,7 @@ class BC:
         print("[", self.block_data, "|", "New Block Hash :", self.block_hash, "]")
 
 
+# simple function for input validation
 def check_option(answer):
     if not option.isdigit() or int(option) <= 0 or int(option) > 3:
         print("not an option")
@@ -56,71 +57,92 @@ def check_option(answer):
         return True
 
 
-# prints what the product information should look like to the user
-# creates the initial product and adds it into the blockchain, checks if it is fake
+# 61 - 63 Creates the Initial token
 VideoGame = BC("Initialized", "CryptoMart", "VideoGame", "CryptoMart")
 Book = BC("Initialized", "CryptoMart", "Book", "CryptoMart")
 Monkey_NFT = BC("Initialized", "CryptoMart", "Monkey NFT", "CryptoMart")
 print("Welcome to CryptoMart, A secure online market!")
 print("Packets look like --> ['Company' : 'Item' : 'Owner' | Old Hash | New Block Hash]")
 
+# Input variable that also acts as a digital signature
 name = input("First what is your name?\n>> ")
 print("Hi " + name + ", What would you like to buy? ")
 print("Here is our stock!")
 print("1:", VideoGame.item, "\n2:", Book.item, "\n3:", Monkey_NFT.item)
 option = input(">> ")
 
+# While loop for input validation to make sure input is valid
 while not check_option(option):
     option = input(">> ")
 
+# if check option is accepted
 if check_option(option):
+    # option 1 check
     if option == "1":
         print("Oh, a ", VideoGame.item, "!\nHere's the Block with the Hashes!")
+        # creates the new book that you bought and dispays the packet
         new_videogame = BC(VideoGame.block_hash, "CryptoMart", "VideoGame", name)
         new_videogame.displayProduct()
 
+        # fake video game that the "hacker" tried to steal while you bought it
         fake_vg = BC(VideoGame.block_hash, "CryptoMart", "Book", "Hacker")
         print("Looks like a hacker is trying to take your new Video Game!",
               "\nFortunately, we can compare the hashes to check the integrity")
         print("Your Hash:\t\t", new_videogame.block_hash, "\nHacker's Hash:\t", fake_vg.block_hash)
+
+        # creates the predicted hash that the Crypto store is anticipating
         needed_hash = BC.predictHash("", "CryptoMart", "VideoGame", name, VideoGame.block_hash)
         print("Needed Hash:\t", needed_hash)
 
+        # if the needed hash is the same as your hash then it's accepted
         if needed_hash == new_videogame.block_hash:
-            print("Your's is the correct Packet!")
+            print("Your hash, is the accepted Hash!")
         else:
-            print("Error SOMETHING WENT WRONG")
+            print("Error, your hash was not the right hash.")
 
+    # option 2 check
     if option == "2":
         print("Oh, a ", Book.item, "!\nHere's the Block with the Hashes!")
+        # creates the new book that you bought and dispays the packet
         new_book = BC(Book.block_hash, "CryptoMart", "Book", name)
         new_book.displayProduct()
 
+        # fake book that the "hacker" tried to steal while you bought it
         fake_book = BC(Book.block_hash, "CryptoMart", "Book", "Hacker")
         print("Looks like a hacker is trying to take your new Book!",
               "\nFortunately, we can compare the hashes to check the integrity")
         print("Your Hash:\t\t", new_book.block_hash, "\nHacker's Hash:\t", fake_book.block_hash)
+
+        # creates the predicted hash that the Crypto store is anticipating
         needed_hash = BC.predictHash("", "CryptoMart", "Book", name, Book.block_hash)
         print("Needed Hash:\t", needed_hash)
 
+        # if the needed hash is the same as your hash then it's accepted
         if needed_hash == new_book.block_hash:
-            print("Your's is the correct Packet!")
+            print("Your hash, is the accepted Hash!")
         else:
-            print("Error SOMETHING WENT WRONG")
+            print("Error your hash, was not the right hash.")
 
+    # option 3 check
     if option == "3":
         print("Oh, a ", Monkey_NFT.item, "!\nHere's the Block with the Hashes!")
+        # creates the new book that you bought and dispays the packet
         new_monkey_nft = BC(Monkey_NFT.block_hash, "CryptoMart", "Monkey NFT", name)
         new_monkey_nft.displayProduct()
 
+        # fake NFT that the "hacker" tried to steal while you bought it
         fake_nft = BC(Book.block_hash, "CryptoMart", "Monkey NFT", "Hacker")
         print("Looks like a hacker is trying to take your new Book!",
               "\nFortunately, we can compare the hashes to check the integrity")
         print("Your Hash:\t\t", new_monkey_nft.block_hash, "\nHacker's Hash:\t", fake_nft.block_hash)
+
+        # creates the predicted hash that the Crypto store is anticipating
         needed_hash = BC.predictHash("", "CryptoMart", "Monkey NFT", name, Monkey_NFT.block_hash)
         print("Needed Hash:\t", needed_hash)
+
+        # if the needed hash is the same as your hash then it's accepted
         if needed_hash == new_monkey_nft.block_hash:
-            print("Your's is the correct Packet!")
+            print("Your hash, is the accepted Hash!")
         else:
-            print("Error SOMETHING WENT WRONG")
+            print("Error your hash, was not the right hash.")
 
